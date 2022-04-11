be9e9e2ac093ffefaed6c0b5f2494752282ee2fe
PawelFryc <pawelfryc@gmail.com>
2022-04-11 14:45:54 +0200

    Throw Dice

---
+++ index.js
@@ -0,0 +1,44 @@
+const diceDropdown = document.querySelector("#number-of-sides");
+const throwButton = document.querySelector(".throw-button");
+const modifierInput = document.querySelector("#modifier");
+const resultsList = document.querySelector(".wyniki");
+const clearButton = document.querySelector(".clear");
+const customDiceInput = document.querySelector("#choose-dice");
+
+function addThrowResult(result, numberOfSides) {
+  const element = document.createElement("li");
+  element.innerHTML = `k${numberOfSides} = ${result}`;
+  resultsList.appendChild(element);
+}
+
+throwButton.addEventListener("click", function () {
+  // let numberOfSides = diceReference.value;
+  // if (diceReference.value === "custom") {
+  //   numberOfSides = customTypeReference.value;
+  // }
+  //
+  const numberOfSides =
+    diceDropdown.value === "custom"
+      ? customDiceInput.value
+      : diceDropdown.value;
+  //turnary operator
+  const result = Math.ceil(Math.random() * numberOfSides);
+  addThrowResult(result, numberOfSides);
+  //Chujowy sposób
+  // resultsReference.innerHTML += `<li>${result}</li>`;
+});
+
+clearButton.addEventListener("click", function () {
+  resultsList.innerHTML = "";
+});
+
+diceDropdown.addEventListener("change", function () {
+  // if (diceReference.value === "custom") {
+  //   customTypeReference.disabled = false;
+  // }
+  // else {
+  //   customTypeReference.disabled = true;
+  // }
+  // customTypeReference.disabled = !(diceReference.value === "custom");
+  customDiceInput.disabled = diceDropdown.value !== "custom";
+});
